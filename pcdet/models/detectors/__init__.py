45fcc06cfaf8c102ccbb8bc9422996f3c8e8a086
brick <33474693+azhuantou@users.noreply.github.com>
2023-04-05 16:24:39 +0800

    code release

---
+++ pcdet/models/detectors/__init__.py
@@ -0,0 +1,37 @@
+from .detector3d_template import Detector3DTemplate
+from .PartA2_net import PartA2Net
+from .point_rcnn import PointRCNN
+from .pointpillar import PointPillar
+from .pv_rcnn import PVRCNN
+from .pv_rcnn_ssl import PVRCNNSSL
+from .second_net import SECONDNet
+from .second_net_iou import SECONDNetIoU
+from .caddn import CaDDN
+from .voxel_rcnn import VoxelRCNN
+from .voxel_rcnn_ssl import VoxelRCNNSSL
+from .centerpoint import CenterPoint
+from .pv_rcnn_plusplus import PVRCNNPlusPlus
+
+__all__ = {
+    'Detector3DTemplate': Detector3DTemplate,
+    'SECONDNet': SECONDNet,
+    'PartA2Net': PartA2Net,
+    'PVRCNN': PVRCNN,
+    'PVRCNNSSL': PVRCNNSSL,
+    'PointPillar': PointPillar,
+    'PointRCNN': PointRCNN,
+    'SECONDNetIoU': SECONDNetIoU,
+    'CaDDN': CaDDN,
+    'VoxelRCNN': VoxelRCNN,
+    'VoxelRCNNSSL': VoxelRCNNSSL,
+    'CenterPoint': CenterPoint,
+    'PVRCNNPlusPlus': PVRCNNPlusPlus
+}
+
+
+def build_detector(model_cfg, num_class, dataset):
+    model = __all__[model_cfg.NAME](
+        model_cfg=model_cfg, num_class=num_class, dataset=dataset
+    )
+
+    return model
